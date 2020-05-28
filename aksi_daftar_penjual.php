a5bb06ea417a3fc3b553b06eb6ee55822d766d58
izzatihusna <61407733+izzatihusna@users.noreply.github.com>
2020-05-28 21:37:24 +0700

    Update aksi_daftar_penjual.php

--- aksi_daftar_penjual.php
+++ aksi_daftar_penjual.php
@@ -6,7 +6,7 @@ $nama 	= $_POST['nama'];
 $email 	= $_POST['email'];
 $password= $_POST['password'];
 
-mysqli_query($koneksi, "INSERT INTO pengguna VALUES ('', '$nama', '$email', '$password', 'Penjual', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '')");
+mysqli_query($koneksi, "INSERT INTO pengguna VALUES ('', '$nama', '$email', '$password', 'Penjual', '', '0', '', '', '', '', '')");
 
 header("location:masuk.php?status=berhasil");
 
