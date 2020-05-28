8b955187574fe9298e8820cfd061d4682d160d46
RizkiNabilahSilmina05 <63065064+RizkiNabilahSilmina05@users.noreply.github.com>
2020-05-28 21:54:45 +0700

    Update aksi_daftar_pembeli.php

--- aksi_daftar_pembeli.php
+++ aksi_daftar_pembeli.php
@@ -9,7 +9,6 @@ $password= $_POST['password'];
 $data = mysqli_query($koneksi, "INSERT INTO pengguna VALUES ('', '$nama', '$email', '$password', 'Pembeli', '', '0', '', '', '', '', '')");
 $ambil = mysqli_fetch_array($data);
 
-
 header("location:masuk.php?status=berhasil");
 
 ?>
